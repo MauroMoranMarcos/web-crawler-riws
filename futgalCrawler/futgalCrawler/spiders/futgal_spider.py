985e946a2e8c1ab14ec6d92cfe868ff3cc534715
Manuel Pérez Sueiro <manuel.psueiro@udc.es>
2024-10-18 17:28:38 +0200

    Añadido crawleo de partidos de jornadas, con información de los partidos

--- futgalCrawler/futgalCrawler/spiders/futgal_spider.py
+++ futgalCrawler/futgalCrawler/spiders/futgal_spider.py
@@ -17,18 +17,27 @@ class FutgalSpiderSpider(scrapy.Spider):
     name = "futgal_spider"
     allowed_domains = ["www.futgal.es"]
     start_urls = ["https://www.futgal.es/pnfg/NPcd/NFG_CmpJornada?cod_primaria=1000120&CodCompeticion=20005937&CodGrupo=22011727&CodTemporada=20&CodJornada=6&Sch_Codigo_Delegacion=3&codigo_tipo_juego=1"]
-
+    #start_urls = ["https://www.futgal.es/pnfg/NPcd/NFG_CmpJornada?cod_primaria=1000120&CodCompeticion=20005937&CodGrupo=22011727&CodTemporada=20&CodJornada=4&Sch_Codigo_Delegacion=3&Sch_Tipo_Juego="]
+    
     def parse(self, response):
-        partidos = response.xpath('//table/tbody/tr[td]')
+        partidos = response.xpath('//table')
 
-        for partido in partidos:
+        for index, partido in enumerate(partidos):
+
+            if index == 0:
+                continue
+
             match = MatchItem()
             match['home_team'] = partido.xpath('.//td[1]//a/text()').get()
             match['away_team'] = partido.xpath('.//td[3]//a/text()').get()
             match['date'] = partido.xpath('.//span[contains(@class, "horario")][1]/text()').get()
             match['time'] = partido.xpath('.//span[contains(@class, "horario")][2]/text()').get()
-            match['field'] = partido.xpath('.//td[contains(@colspan, "9")]//a/text()').get()
-            match['referee'] = partido.xpath('.//td[contains(@colspan, "9")]//strong/text()').get()
+            match['field'] = partido.xpath('.//tr[2]/td[contains(@colspan, "9")]//a/text()').get()
+            
+            raw_referee = partido.xpath('.//tr[2]/td[contains(@colspan, "9")]//span[@class= "font_widgetL"]/text()').getall()
+            clean_referee = [a.strip() for a in raw_referee if a.strip()]
+
+            match['referee'] = clean_referee[-1] if clean_referee else None
 
             yield match
 
